ab0d4f28004548477f30c276618616a738d870c6
Rajeev Singh <rjvsingh88@gmail.com>
2023-12-26 01:19:01 +0530

    added rest controller class

---
+++ test/src/main/java/test/Restcontroller.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Restcontroller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("added a rest controller class");
+		
+
+	}
+
+}
