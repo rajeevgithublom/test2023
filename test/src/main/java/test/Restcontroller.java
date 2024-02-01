d43c2179243d2a6e8b3e82e23dfd3011bc61c110
Rajeev Singh <rjvsingh88@gmail.com>
2024-02-01 20:44:42 +0530

    int a added in sanity

--- test/src/main/java/test/Restcontroller.java
+++ test/src/main/java/test/Restcontroller.java
@@ -5,6 +5,7 @@ public class Restcontroller {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	int b;	
+	int a;
 	System.out.println("removed everything from owner");
 	}
 
