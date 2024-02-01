8574559d7255b40a11eec236e3a380da8cc6eb4b
Rajeev Singh <rjvsingh88@gmail.com>
2024-02-01 20:32:59 +0530

    added j variable

--- test/src/main/java/test/Restcontroller.java
+++ test/src/main/java/test/Restcontroller.java
@@ -4,7 +4,7 @@ public class Restcontroller {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+	int j;	
 	System.out.println("removed everything from owner");
 	}
 
