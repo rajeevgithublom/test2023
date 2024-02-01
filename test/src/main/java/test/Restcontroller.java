18c42903619f0677c70f6a6ae959bffd81bffd76
Rajeev Singh <rjvsingh88@gmail.com>
2024-02-01 20:59:49 +0530

    int and string both names added

--- test/src/main/java/test/Restcontroller.java
+++ test/src/main/java/test/Restcontroller.java
@@ -4,24 +4,16 @@ public class Restcontroller {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 
-	int a;	
-    int b;
-    int c;
-=======
-	int b;	
-	int a;
-	int c;
->>>>>>> refs/heads/sanity
-	int d;
-<<<<<<< HEAD
-	String name;
+		int a;
+		int b;
+		int c;
+		int d;
+		String name;
 
-=======
-	int name;
->>>>>>> refs/heads/sanity
-	System.out.println("removed everything from owner");
+		int names;
+
+		System.out.println("removed everything from owner");
 	}
 
 }
